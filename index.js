9835911b393468c2470f186b5508802cb7827eac
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-18 18:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter;
+
+  function addBump() {
+    counter += 1;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+}
+
+function createAnimal(animalType) {
+
+  function deadlyDevice(deadlyDevice) {
+    var obj = {}
+
+    obj["animalType"] = animalType;
+    obj["deadlyDevice"] = deadlyDevice;
+
+    return obj;
+  }
+
+  
+}
